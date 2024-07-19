3d2381fb0651f34568f863c47d6b7ce23b6d2e90
Avishkar-Kakade <kakadeavishkar84@gmail.com>
2024-07-20 02:42:19 +0530

    cgh

--- backend/utilities.js
+++ backend/utilities.js
@@ -23,7 +23,7 @@ const User = require('../backend/models/user.model');
 
 exports.authenticateToken = async (req, res, next) => {
     try {
-        console.log("token object" + req.cookies)
+        console.log("token object" + JSON.stringify(req.cookies))
         const token = req.cookies.token; // Get the token from cookies
         console.log("your token is: " + token);
         const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET
